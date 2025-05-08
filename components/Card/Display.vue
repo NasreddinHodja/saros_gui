c9d83b58bb0b4f2ecf8ba6c7a10c16385cee095d
tbizetde@gmail.com <tbizetde@gmail.com>
2025-05-08 18:26:48 -0300

    small ui fixes

--- components/Card/Display.vue
+++ components/Card/Display.vue
@@ -6,7 +6,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="p-4 flex flex-col items-center h-[50vh]">
+  <div class="p-4 flex flex-col items-center justify-around flex- h-[50vh]">
     <div class="w-full h-[90%]">
       <img :src="path" :alt="name" class="w-full h-full object-contain" />
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -16,24 +16,8 @@ const clearDrawedCards = () => {
   <div
     class="h-full w-full flex flex-col gap-4 items-center justify-center bg-black"
   >
-    <div class="flex flex-row space-x-4">
-      <div
-        v-for="card in drawedCards"
-        :key="card.path"
-        class="flex-shrink-0 flex-grow-0 basis-1/5"
-        :style="{
-          flexBasis:
-            drawedCards.length === 1
-              ? '100%'
-              : drawedCards.length === 2
-              ? '50%'
-              : drawedCards.length === 3
-              ? '33%'
-              : drawedCards.length === 4
-              ? '25%'
-              : '20%',
-        }"
-      >
+    <div class="flex flex-row gap-4 max-w-screen">
+      <div v-for="card in drawedCards" :key="card.path">
         <CardDisplay
           :name="card.name"
           :path="card.path"
