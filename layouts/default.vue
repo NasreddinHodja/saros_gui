6bfb165b058c9a637c1480db2942c2bc3d9de395
tbizetde@gmail.com <tbizetde@gmail.com>
2025-06-24 18:26:50 -0300

    metadata and favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) =>
+    titleChunk ? `${titleChunk} - S.E.E.D.` : "S.E.E.D.",
+  meta: [
+    { property: "og:title", content: "S.E.E.D. SAROS" },
+    { property: "og:description", content: "S.E.E.D. Tarot Bot" },
+    { property: "og:image", content: "/seed_logo.png" },
+    { property: "og:type", content: "website" },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:image", content: "/seed_logo.png" },
+  ],
+});
+</script>
 
 <template>
   <div
